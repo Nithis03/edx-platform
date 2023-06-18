0ba3c6f460926b8467a7517440a8b97886f3e00b
Shahbaz Shabbir <shbzshbr@gmail.com>
2023-06-19 03:49:23 +0500

    fix: edx-ace templates and send email functionality

--- common/djangoapps/student/management/commands/retrieve_unsubscribed_emails.py
+++ common/djangoapps/student/management/commands/retrieve_unsubscribed_emails.py
@@ -5,9 +5,16 @@ import logging
 import uuid
 from datetime import datetime, timedelta
 
+from django.contrib.auth.models import User  # lint-amnesty, pylint: disable=imported-auth-user, wrong-import-order
+from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand, CommandError
+from edx_ace import ace
+from edx_ace.recipient import Recipient
 
+from common.djangoapps.student.message_types import RetrieveUnsubscribedEmails
 from lms.djangoapps.utils import get_braze_client
+from openedx.core.djangoapps.ace_common.template_context import get_base_template_context
+from openedx.core.lib.celery.task_utils import emulate_http_request
 
 logger = logging.getLogger(__name__)
 
@@ -57,7 +64,8 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.SUCCESS('Unsubscribed emails retrieved successfully'))
         except Exception as exc:  # pylint: disable=broad-except
             logger.exception(f'Unable to retrieve unsubscribed emails from Braze due to : {exc}')
-            raise CommandError(f'Unable to retrieve unsubscribed emails from Braze due to : {exc}')  # lint-amnesty, pylint: disable=raise-missing-from
+            raise CommandError(
+                f'Unable to retrieve unsubscribed emails from Braze due to : {exc}')  # lint-amnesty, pylint: disable=raise-missing-from
 
         output_filename = f'{str(uuid.uuid4())}.csv'
 
@@ -67,9 +75,33 @@ class Command(BaseCommand):
                 csv_writer.writerow(('email', 'unsubscribed_at'))
                 rows = [(email['email'], email['unsubscribed_at']) for email in emails]
                 csv_writer.writerows(rows)
+                csv_file_path = output_file.name
                 self.stdout.write(
                     self.style.SUCCESS(f'Unsubscribed emails write in CSV file {output_filename} successfully'))
         except OSError as e:  # pylint: disable=broad-except
             logger.exception(f'Error writing to file: {output_filename}')
             raise CommandError(
                 f'Error writing to file: {output_filename}') from e  # lint-amnesty, pylint: disable=raise-missing-from
+
+        try:
+            site = Site.objects.get_current()
+            message_context = get_base_template_context(site)
+            message_context.update({
+                'start_date': start_date,
+                'end_date': end_date,
+                'csv_file_path': csv_file_path
+            })
+
+            user = User.objects.get(pk=19)
+            with emulate_http_request(site=site, user=user):
+                msg = RetrieveUnsubscribedEmails().personalize(
+                    recipient=Recipient(user.id, user.email),
+                    language='English',
+                    user_context=message_context,
+                )
+
+                ace.send(msg)
+                print("\n\n\nGOT IT\n\n\n")
+                logger.info('Unsubscribed emails data sent to your email.')
+        except Exception:  # pylint: disable=broad-except
+            logger.exception('Could not send email')
